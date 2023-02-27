c04679ae70874ed2aa4d17c61e757933104da67e
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-02-27 18:55:20 +0100

    Added foreign key from Card to Template and improved Card serialization (__str__ method).

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -6,25 +6,7 @@ from .utils.model_mixins import TriggeredUpdatesMixin
 encoding = CardsConfig.default_encoding
 
 
-class Card(models.Model, TriggeredUpdatesMixin):
-    __hashed_fields__ = ('front', 'back',)
-
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4(),
-        editable=False
-    )
-    last_modified = models.DateTimeField(auto_now=True)
-    hash = models.CharField(max_length=64, unique=True)
-    front = models.TextField()
-    back = models.TextField()
-
-    def __str__(self):
-        return f"Q: {self.front}; A: {self.back}"
-
-
 class Template(models.Model, TriggeredUpdatesMixin):
-    # TODO: start with foreign key from the card to the template
     __hashed_fields__ = ("title", "description", "body",)
     id = models.UUIDField(
         primary_key=True,
@@ -40,3 +22,28 @@ class Template(models.Model, TriggeredUpdatesMixin):
     def __str__(self):
         return f"<{self.title}>"
 
+
+class Card(models.Model, TriggeredUpdatesMixin):
+    __hashed_fields__ = ('front', 'back',)
+
+    # constant used in object serialization
+    MAX_LEN = 50
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4(),
+        editable=False
+    )
+    last_modified = models.DateTimeField(auto_now=True)
+    hash = models.CharField(max_length=64, unique=True)
+    front = models.TextField()
+    back = models.TextField()
+    template = models.ForeignKey(Template, on_delete=models.PROTECT,
+                                 null=True, related_name="cards")
+
+    def __str__(self):
+        serialized = f"Q: {self.front}; A: {self.back}"
+        if len(serialized) > Card.MAX_LEN:
+            serialized = serialized[:Card.MAX_LEN] + " ..."
+
+        return serialized
--- backend/cards/tests.py
+++ backend/cards/tests.py
@@ -1,4 +1,5 @@
-import uuid
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.deletion import ProtectedError
 from django.test import TestCase
 from .models import Card, Template
 import hashlib
@@ -10,19 +11,19 @@ fake = Faker()
 # Create your tests here.
 class TemplateModelTests(TestCase):
     def setUp(self):
-        self.title = fake.text(60)
+        self.template_title = fake.text(60)
         self.description = fake.text(300)
         self.body = fake.text(300)
 
         self.template = Template.objects.create(
-            title=self.title,
+            title=self.template_title,
             description=self.description,
             body=self.body
         )
 
     def test_template_hashing(self):
         template_hash = hashlib.sha256(
-            bytes(self.title + self.description + self.body, "utf-8")
+            bytes(self.template_title + self.description + self.body, "utf-8")
         ).hexdigest()
         self.assertEqual(template_hash, self.template.hash)
 
@@ -36,7 +37,7 @@ class TemplateModelTests(TestCase):
         self.assertNotEqual(self.template.last_modified, prev_last_modified)
 
     def test_serialization(self):
-        expected_serialization = f"<{self.title}>"
+        expected_serialization = f"<{self.template_title}>"
         actual_serialization = str(self.template)
         self.assertEqual(actual_serialization, expected_serialization)
 
@@ -70,3 +71,50 @@ class CardModelTests(TestCase):
                                   + " answer.")
         actual_serialization = str(self.card)
         self.assertEqual(actual_serialization, expected_serialization)
+
+
+class TemplateCardRelationshipTests(TemplateModelTests, CardModelTests):
+    def setUp(self):
+        TemplateModelTests.setUp(self)
+        CardModelTests.setUp(self)
+        card = Card.objects.first()
+        card.template = Template.objects.first()
+        card.save()
+
+    def test_add_template_to_card(self):
+        card, template = self._get_tested_objects()
+        card.template = template
+        card.save()
+
+        self.assertTrue(card.template is template)
+        self.assertTrue(card.template_id == template.id)
+
+    def _get_tested_objects(self):
+        card = Card.objects.first()
+        template = Template.objects.first()
+
+        return card, template
+
+    def test_remove_template_from_card(self):
+        card, template = self._get_tested_objects()
+        card.template = None
+        card.save()
+
+        self.assertFalse(card.template is template)
+        self.assertTrue(Template.objects.get(title=self.template_title))
+        self.assertFalse(card.template)
+        self.assertFalse(card.template_id == template.id)
+
+    def test_removing_template(self):
+        card, template = self._get_tested_objects()
+
+        self.assertRaises(ProtectedError, template.delete)
+
+        # remove reference to the template and delete it:
+        card.template = None
+        card.save()
+        template.delete()
+
+        self.assertRaises(
+            ObjectDoesNotExist,
+            lambda: Template.objects.get(title=self.template_title))
