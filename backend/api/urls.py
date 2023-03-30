bafaa4e294307ba73a2829b180e67cd1f434141e
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-03-31 00:47:42 +0200

    Reformatted api.urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SingleCardForBackendView, ListCardsForBackendView, SingleCardForUser
+from .views import (SingleCardForBackendView, ListCardsForBackendView,
+                    SingleCardForUser)
 
 urlpatterns = [
-    path("cards/<uuid:pk>/", SingleCardForBackendView.as_view(), name="single_card"),
+    path("cards/<uuid:pk>/", SingleCardForBackendView.as_view(),
+         name="single_card"),
     path("cards/", ListCardsForBackendView.as_view(), name="list_cards"),
-    path("cards/<uuid:card_pk>/users/<uuid:user_pk>/", SingleCardForUser.as_view(),
-         name="card_for_user"),
+    path("cards/<uuid:card_pk>/users/<uuid:user_pk>/",
+         SingleCardForUser.as_view(), name="card_for_user"),
 ]
