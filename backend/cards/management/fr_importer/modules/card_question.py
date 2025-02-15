0def490b5b0bdc2ccbdfe1291f6e9eec6d048515
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-02-15 21:40:09 +0100

    Removed redundant __init__() from the Question class.

--- backend/cards/management/fr_importer/modules/card_question.py
+++ backend/cards/management/fr_importer/modules/card_question.py
@@ -2,11 +2,5 @@ from cards.management.fr_importer.modules.card_side import CardSide
 
 
 class Question(CardSide):
-    def __init__(self, question):
-        """
-        question - contents of <item><q></q></item> (without <q></q> tags).
-        """
-        super().__init__(question)
-
     definition = property(lambda self: self._get_line(0))
     examples = property(lambda self: self._get_examples(from_line=1))
