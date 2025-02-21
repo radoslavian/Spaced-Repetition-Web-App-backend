1d79376fe40392d982097bacc1ce22632c577570
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-02-22 00:36:21 +0100

    Added type hints in HtmlFormattedMemorizedCard.

--- backend/cards/management/fr_importer/modules/html_memorized_card.py
+++ backend/cards/management/fr_importer/modules/html_memorized_card.py
@@ -4,18 +4,18 @@ from cards.management.fr_importer.modules.user_review import UserReview
 
 
 class HtmlFormattedMemorizedCard(HtmlFormattedCard):
-    def __init__(self, card, time_of_start):
-        card_copy = {**card}
+    def __init__(self, card_details: dict, time_of_start: int):
+        card_copy = {**card_details}
         review_details = card_copy.pop("review_details")
         super().__init__(card_copy)
         self._review_details = UserReview(review_details, time_of_start)
 
     @staticmethod
-    def keys():
+    def keys() -> list[str]:
         keys = HtmlFormattedCard.keys()
         return [*keys, "review_details"]
 
-    def values(self):
+    def values(self) -> list[dict]:
         values = super().values()
         return [*values, dict(self._review_details)]
 
