dfd449abfd418d6de4da0e6874a0ead485076441
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-02-27 00:23:41 +0100

    Removed now redundant ImportedMemorizedCard.get_review_details.

--- backend/cards/management/fr_importer/items_importer/modules/imported_memorized_card.py
+++ backend/cards/management/fr_importer/items_importer/modules/imported_memorized_card.py
@@ -2,8 +2,6 @@ from cards.management.fr_importer.items_importer.modules.imported_card import \
     ImportedCard
 from cards.management.fr_importer.items_parser.modules.html_memorized_card import \
     HtmlFormattedMemorizedCard
-from cards.management.fr_importer.items_parser.modules.user_review import \
-    UserReview
 from cards.models import CardUserData
 from users.models import User
 
@@ -12,8 +10,8 @@ class ImportedMemorizedCard(ImportedCard):
     def __init__(self, card_object: HtmlFormattedMemorizedCard, user: User):
         super().__init__(card_object)
         super().save()
-        review_details = self.get_review_details(card_object["review_details"])
-        self._review_details = self.set_review_details(review_details, user)
+        self._review_details = self.set_review_details(
+            card_object["review_details"], user)
 
     def set_review_details(self, review_details, user):
         user_review = CardUserData(card=self._card,
@@ -21,22 +19,9 @@ class ImportedMemorizedCard(ImportedCard):
                                    **review_details)
         user_review.save()
 
-        # overwriting auto_now_add
+        # overwriting auto_now_add, won't work otherwise
         CardUserData.objects.filter(
             user=user,
             card=self._card
         ).update(introduced_on=review_details["introduced_on"])
         return user_review
-
-    @staticmethod
-    def get_review_details(user_review: UserReview) -> dict:
-        """
-        computed_interval in days has to be fixed into Integer as long as
-        CardUserData.computed_interval field doesn't store timedelta data.
-        """
-        # CardUserData.computed_interval type has to be changed
-        # into DurationField.
-        computed_interval_days = user_review["computed_interval"]
-        review_details = {**user_review,
-                          "computed_interval": computed_interval_days}
-        return review_details
