1ea65f65edc020b33519935edaff28ea63fc515d
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-02-27 01:16:22 +0100

    Adding fullrecall's data about user reviews into database.

--- backend/cards/management/fr_importer/items_importer/tests/test_imported_memorized_card.py
+++ backend/cards/management/fr_importer/items_importer/tests/test_imported_memorized_card.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -43,7 +41,7 @@ class AddingMemorizedCard(TestCase):
             card=cls.database_card,
             user=cls.user)
         cls.user_review = UserReview(cls.card_review_details,
-                                      cls.time_of_start)
+                                     cls.time_of_start)
 
     def test_lapses(self):
         expected_lapses = 0
@@ -73,26 +71,75 @@ class AddingMemorizedCard(TestCase):
         self.assertEqual(expected_introduced_on, received_introduced_on)
 
     def test_last_reviewed(self):
-        pass
+        expected_last_reviewed = self.user_review.last_reviewed.date()
+        received_last_reviewed = self.database_user_review.last_reviewed
+        self.assertEqual(expected_last_reviewed, received_last_reviewed)
 
     def test_computed_interval(self):
-        pass
+        expected_computed_interval = self.user_review.computed_interval
+        received_computed_interval = (self.database_user_review.
+                                      computed_interval)
+        self.assertEqual(expected_computed_interval,
+                         received_computed_interval)
 
-    def test_last_real_interval(self):
-        # can't be set, test only
-        pass
+    # def test_current_real_interval(self):
+    #     # since this is a calculated property and its first argument is
+    #     # current date, only 'last_reviewed' must be tested
+    #     pass
 
     def test_easiness_factor(self):
-        pass
+        expected_ef = self.user_review.easiness_factor
+        received_ef = self.database_user_review.easiness_factor
+        self.assertEqual(expected_ef, received_ef)
+
+
+class CramTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.card_review_details = {
+            "id": "1236435838",
+            "tmtrpt": "6574",
+            "stmtrpt": "6574",
+            "livl": "1274",
+            "rllivl": "1764",
+            "ivl": "583",
+            "rp": "6",
+            "gr": "4"
+        }
+        User = get_user_model()
+        cls.user = User(username="user")
+        cls.user.save()
+        cls.time_of_start = 1186655166
 
     def test_crammed_false(self):
         """
         Shouldn't be added to the cram queue.
         """
-        pass
+        card_data = {
+            "question": "question",
+            "answer": "answer",
+            "review_details": self.card_review_details
+        }
+        card = HtmlFormattedMemorizedCard(card_data, self.time_of_start)
+        ImportedMemorizedCard(card, self.user)
+        database_card = Card.objects.first()
+        database_user_review = CardUserData.objects.get(user=self.user,
+                                                        card=database_card)
+        self.assertFalse(database_user_review.crammed)
 
     def test_crammed_true(self):
         """
         Should be added to the cram queue.
         """
-        pass
+        card_review_details = {**self.card_review_details, "gr": "1"}
+        card_data = {
+            "question": "question",
+            "answer": "answer",
+            "review_details": card_review_details
+        }
+        card = HtmlFormattedMemorizedCard(card_data, self.time_of_start)
+        ImportedMemorizedCard(card, self.user)
+        database_card = Card.objects.first()
+        database_user_review = CardUserData.objects.get(user=self.user,
+                                                        card=database_card)
+        self.assertTrue(database_user_review.crammed)
