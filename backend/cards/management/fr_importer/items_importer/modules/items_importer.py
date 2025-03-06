ecce995c5cf3e1126097d8142424b041efe9456d
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-03-06 22:48:31 +0100

    ItemsImporter.set_import_category_xpath doesn't set local ._xpath field - only sets ItemsParser.import_xpath.

--- backend/cards/management/fr_importer/items_importer/modules/items_importer.py
+++ backend/cards/management/fr_importer/items_importer/modules/items_importer.py
@@ -11,7 +11,6 @@ from cards.models import Category, CardTemplate
 
 class ItemsImporter:
     def __init__(self, elements_path: str):
-        self._category_xpath = None
         self._template = None
         self._categories = None
         self._items_parser = ItemsParser(elements_path)
@@ -20,9 +19,6 @@ class ItemsImporter:
         """
         Uploads items from FullRecall's elements.xml into the database.
         """
-        if self._category_xpath:
-            self._items_parser.import_xpath = self._category_xpath
-
         for card_to_import in self._items_parser:
             imported_card = self.import_single_card(card_to_import)
             self.configure_imported_card(imported_card)
@@ -68,7 +64,7 @@ class ItemsImporter:
         """
         Set category (in elements.xml) xpath from which items are imported.
         """
-        self._category_xpath = xpath
+        self._items_parser.import_xpath = xpath
 
 
 class PendingItemsImporter(ItemsImporter):
