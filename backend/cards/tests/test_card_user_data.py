c9aec46f4ba0ae9e9ae65b821cfe51667399ec04
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-07-13 22:55:53 +0200

    Refactor the CramQueue class to separate several tests within it (from the single one that is already there).

--- backend/cards/tests/test_card_user_data.py
+++ backend/cards/tests/test_card_user_data.py
@@ -1,36 +1,54 @@
 from django.test import TestCase
-from cards.models import CardUserData
 from cards.tests.fake_data import make_fake_cards, make_fake_users
 
 
 class CramQueue(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.low_grade, cls.high_grade = 2, 5
+        cls.card_1, cls.card_2, cls.card_3 = make_fake_cards(3)
+        [cls.user] = make_fake_users(1)
+
     def setUp(self):
-        self.card_1, self.card_2, self.card_3 = make_fake_cards(3)
+        self.crammed_card = self.card_1.memorize(self.user, self.low_grade)
+        self.not_crammed_card = self.card_2.memorize(self.user,
+                                                     self.high_grade)
+        self.reference_card = self.card_3.memorize(self.user, self.low_grade)
 
-    def test_cram(self):
-        user = make_fake_users(1)[0]
-        # TODO: split into several tests
-        # TODO: following should go into setUp
-        # memorizing adds cards to cram
-        for card in self.card_1, self.card_2, self.card_3:
-            card.memorize(user, 3)
+    def test_user_crammed_cards(self):
+        """
+        User.crammed_cards reports number of cards in the cram queue.
+        """
+        expected_number_of_crammed_cards = 2
 
-        self.assertEqual(len(user.crammed_cards), 3)
+        self.assertEqual(len(self.user.crammed_cards),
+                         expected_number_of_crammed_cards)
 
-        # removing from cram - status change
-        card_review_data = CardUserData.objects.get(user=user,
-                                                    card=self.card_1)
-        self.assertTrue(card_review_data.crammed)
-        status = card_review_data.remove_from_cram()
+    def test_removing_crammed_card(self):
+        """
+        Removing a crammed card from the cram queue changes the crammed status
+        of the card.
+        """
+        self.assertTrue(self.crammed_card.crammed)
+        status = self.crammed_card.remove_from_cram()
+
         self.assertFalse(status)
-        self.assertEqual(len(user.crammed_cards), 2)
-        self.assertFalse(card_review_data.crammed)
+        self.assertEqual(len(self.user.crammed_cards), 1)
+        self.assertFalse(self.crammed_card.crammed)
 
-        # removing from cram - no status change
-        status = card_review_data.remove_from_cram()
-        self.assertFalse(status)  # card wasn't crammed
+    def test_removing_not_crammed_card(self):
+        """
+        An attempt to remove a card from the cram queue shouldn't change the
+        status of the card.
+        """
+        status = self.not_crammed_card.remove_from_cram()
+        self.assertFalse(status)
 
-        # manually adding to cram
-        status = card_review_data.add_to_cram()
+    def test_add_manually(self):
+        """
+        Manually adding a card to the cram queue.
+        """
+        status = self.not_crammed_card.add_to_cram()
+
         self.assertTrue(status)
-        self.assertTrue(card_review_data.crammed)
+        self.assertTrue(self.not_crammed_card.crammed)
