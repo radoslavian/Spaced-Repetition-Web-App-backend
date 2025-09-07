f3c582ed17a48bb676400f43829a6facb7a7c070
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-09-07 22:25:36 +0200

    Move SoundInCards test case into test_sound_model.py and rework it.

--- backend/cards/tests/test_sound_model.py
+++ backend/cards/tests/test_sound_model.py
@@ -35,7 +35,6 @@ class SoundFileModel(TestCase):
 
 class SoundInCards(TestCase):
     def setUp(self):
-        self.card_1, self.card_2 = fake_data_objects.make_fake_cards(2)
         sound_entries = []
         # valid as long as .placeholder_audio_files has only two elements:
         for placeholder_audio in fake_data_objects.placeholder_audio_files:
@@ -47,82 +46,96 @@ class SoundInCards(TestCase):
             })
         self.sound_entry_1 = sound_entries[0]["entry"]
         self.sound_entry_2 = sound_entries[1]["entry"]
+        number_of_cards = 2
+        self.card_1, self.card_2 = fake_data_objects.make_fake_cards(
+            number_of_cards)
 
     def tearDown(self):
-        Sound.objects.all().delete()
+        for Model in [Sound, Card]:
+            Model.objects.all().delete()
 
     def test_sounds_front(self):
-        """Attaching one sound to multiple cards (front).
+        """
+        Attaching one sound to multiple cards (front).
         """
         self.card_1.front_audio = self.sound_entry_1
         self.card_2.front_audio = self.sound_entry_1
         self.card_2.back_audio = self.sound_entry_2
-        self.card_1.save()
-        self.card_2.save()
+        self.save_cards()
 
-        self.assertEqual(self.sound_entry_1.cards_front.count(), 2)
+        number_of_front_cards = 2
+        self.assertEqual(self.sound_entry_1.cards_front.count(),
+                         number_of_front_cards)
         self.assertFalse(self.sound_entry_2.cards_front.all())
 
     def test_sounds_back(self):
-        """Attaching one sound to multiple cards (back).
+        """
+        Attaching one sound to multiple cards (back).
         """
         self.card_1.back_audio = self.sound_entry_1
         self.card_2.back_audio = self.sound_entry_1
-        self.card_2.front_audio = self.sound_entry_1
-        self.card_1.save()
-        self.card_2.save()
+        self.card_2.front_audio = self.sound_entry_2
+        self.save_cards()
 
-        self.assertEqual(self.sound_entry_1.cards_back.count(), 2)
+        number_of_back_cards = 2
+        self.assertEqual(self.sound_entry_1.cards_back.count(),
+                         number_of_back_cards)
         self.assertFalse(self.sound_entry_2.cards_back.all())
 
     def test_related_name_front(self):
         self.card_1.front_audio = self.sound_entry_1
         self.card_2.back_audio = self.sound_entry_1
-        self.card_1.save()
-        self.card_2.save()
+        self.save_cards()
 
-        self.assertEqual(self.sound_entry_1.cards_front.count(), 1)
+        expected_number_of_cards = 1
+        self.assertEqual(self.sound_entry_1.cards_front.count(),
+                         expected_number_of_cards)
         self.assertEqual(self.sound_entry_1.cards_front.first(), self.card_1)
 
     def test_related_name_back(self):
         self.card_1.back_audio = self.sound_entry_1
         self.card_2.front_audio = self.sound_entry_1
-        self.card_1.save()
-        self.card_2.save()
+        self.save_cards()
 
-        self.assertEqual(self.sound_entry_1.cards_back.count(), 1)
+        expected_number_of_cards = 1
+        self.assertEqual(self.sound_entry_1.cards_back.count(),
+                         expected_number_of_cards)
         self.assertEqual(self.sound_entry_1.cards_back.first(), self.card_1)
 
+    def save_cards(self):
+        for card in (self.card_1, self.card_2,):
+            card.save()
+
     def test_removing_sound_front(self):
-        """Removing sound entry attached to card's front doesn't
-        delete the card.
         """
-        self.card_1.back_audio = self.sound_entry_1
-        card_1_front = self.card_1.front
+        Removing a sound entry attached to the card's front doesn't cause
+        card's deletion.
+        """
+        self.card_1.front_audio = self.sound_entry_1
         self.card_1.save()
         self.sound_entry_1.delete()
-        card_from_db = Card.objects.filter(front=card_1_front).first()
+        self.card_1.refresh_from_db()
 
-        self.assertEqual(card_from_db.front, card_1_front)
+        self.assertTrue(self.card_1.id)
         # check if field definition's on_delete=models.SET_NULL works:
-        self.assertIsNone(card_from_db.front_audio)
+        self.assertIsNone(self.card_1.front_audio)
 
     def test_removing_sound_back(self):
-        """Removing sound entry attached to card's back doesn't
-        delete the card.
+        """
+        Removing a sound entry attached to card's back doesn't delete the card.
         """
         self.card_1.back_audio = self.sound_entry_1
-        card_1_back = self.card_1.back
         self.card_1.save()
         self.sound_entry_1.delete()
-        card_from_db = Card.objects.filter(back=card_1_back).first()
+        self.card_1.refresh_from_db()
 
-        self.assertEqual(card_from_db.back, card_1_back)
+        self.assertTrue(self.card_1.id)
         # check if field definition's on_delete=models.SET_NULL works:
-        self.assertIsNone(card_from_db.back_audio)
+        self.assertIsNone(self.card_1.back_audio)
 
     def test_removing_card_front(self):
-        """Removing card doesn't delete sound entry attached
+        """
+        Removing a card doesn't cause deletion of a sound entry attached
         to the card's front.
         """
         self.card_1.front_audio = self.sound_entry_1
@@ -130,10 +143,11 @@ class SoundInCards(TestCase):
         self.card_1.delete()
         self.sound_entry_1.refresh_from_db()
 
-        self.assertTrue(self.sound_entry_1)
+        self.assertTrue(self.sound_entry_1.id)
 
     def test_removing_card_back(self):
-        """Removing card doesn't delete sound entry attached
+        """
+        Removing card doesn't cause deletion of a sound entry attached
         to the card's back.
         """
         self.card_1.back_audio = self.sound_entry_1
