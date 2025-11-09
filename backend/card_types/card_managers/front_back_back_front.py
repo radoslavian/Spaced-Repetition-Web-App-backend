7e1d93b9a4bee5404a2e16f2841e19b0f3f4fab5
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-11-09 17:12:00 +0100

    Implement updating front-back/back-front cards.

--- backend/card_types/card_managers/front_back_back_front.py
+++ backend/card_types/card_managers/front_back_back_front.py
@@ -1,24 +1,55 @@
 import json
+from typing import Dict
+
 from cards.models import Card
 from .manager_abc import CardManager
 
 
 class FrontBackBackFront(CardManager):
     def __init__(self, card_note):
-        self.back_front_card = None
-        self.front_back_card = None
         self.card_note = card_note
+        self.front_back_card = self._get_card_by_id(
+            self.note_metadata.get("front-back-card-id"))
+        self.back_front_card = self._get_card_by_id(
+            self.note_metadata.get("back-front-card-id"))
+
+
+    @property
+    def note_metadata(self) -> Dict:
+        if self.card_note.metadata:
+            return json.loads(self.card_note.metadata)
+        return {}
+
+    def _get_card_by_id(self, card_id):
+        return self.card_note.cards.filter(id=card_id).first()
+
 
     def save_cards(self):
-        data = json.loads(self.card_note.card_description)
-        self.front_back_card = Card.objects.create(front=data["front"]["text"],
-                                                   back=data["back"]["text"],
-                                                   note=self.card_note)
-        self.back_front_card = Card.objects.create(front=data["back"]["text"],
-                                                   back=data["front"]["text"],
-                                                   note=self.card_note)
+        front_back_card = self.front_back_card or Card()
+        self.front_back_card = self._save_card(
+            card=front_back_card,
+            front=self.card_description["front"]["text"],
+            back=self.card_description["back"]["text"])
+
+        back_front_card = self.back_front_card or Card()
+        self.back_front_card = self._save_card(
+            card=back_front_card,
+            front=self.card_description["back"]["text"],
+            back=self.card_description["front"]["text"])
 
         self._save_metadata()
+
+    def _save_card(self, card, front, back):
+        card.front = front
+        card.back = back
+        card.note = self.card_note
+        card.save()
+        return card
+
+    @property
+    def card_description(self):
+        return (self.card_note.card_description and
+                json.loads(self.card_note.card_description))
 
     def _save_metadata(self):
         if not all([self.front_back_card, self.back_front_card]):
--- backend/card_types/tests/test_front_back_back_front.py
+++ backend/card_types/tests/test_front_back_back_front.py
@@ -64,3 +64,94 @@ class CreatingCardsFromNote(TestCase):
 
         self.assertEqual(back_front_card.front, front)
         self.assertEqual(back_front_card.back, back)
+
+
+class UpdatingNote(TestCase):
+    card_description = {
+        "front": {
+            "text": "some front text"
+        },
+        "back": {
+            "text": "some back text"
+        }
+    }
+    updated_description = {
+        "front": {
+            "text": "altered front text"
+        },
+        "back": {
+            "text": "altered back text"
+        }
+    }
+
+    @classmethod
+    def setUpTestData(cls):
+        cls._create_note()
+        cls._save_original_card_ids()
+
+        cls._update_note()
+        cls._save_updated_card_ids()
+
+    @classmethod
+    def _save_updated_card_ids(cls):
+        cls.front_back_card_id = json.loads(
+            cls.note.metadata)["front-back-card-id"]
+        cls.back_front_card_id = json.loads(
+            cls.note.metadata)["back-front-card-id"]
+
+    @classmethod
+    def _save_original_card_ids(cls):
+        cls.original_back_front_card_id = json.loads(
+            cls.note.metadata)["back-front-card-id"]
+        cls.original_front_back_card_id = json.loads(
+            cls.note.metadata)["front-back-card-id"]
+
+    @classmethod
+    def _create_note(cls):
+        card_description = json.dumps(cls.card_description)
+        cls.note = CardNote.objects.create(card_description=card_description,
+                                           card_type="front-back-back-front")
+
+    @classmethod
+    def _update_note(cls):
+        cls.note.card_description = json.dumps(cls.updated_description)
+        cls.note.save()
+
+    def test_number_of_cards(self):
+        """
+        Number of cards shouldn't change.
+        Additional calls to CardNote.save() shouldn't create any new cards.
+        """
+        expected_number = 2
+        received_number = Card.objects.count()
+        self.assertEqual(expected_number, received_number)
+
+    def test_front_back_text(self):
+        card = Card.objects.get(id=self.front_back_card_id)
+        expected_front_text = self.updated_description["front"]["text"]
+        expected_back_text = self.updated_description["back"]["text"]
+
+        self.assertEqual(card.front, expected_front_text)
+        self.assertEqual(card.back, expected_back_text)
+
+    def test_back_front_text(self):
+        card = Card.objects.get(id=self.back_front_card_id)
+        expected_front_text = self.updated_description["back"]["text"]
+        expected_back_text = self.updated_description["front"]["text"]
+
+        self.assertEqual(card.front, expected_front_text)
+        self.assertEqual(card.back, expected_back_text)
+
+    def test_front_back_id(self):
+        """
+        Card id for the front-back card should stay the same.
+        """
+        card = Card.objects.get(id=self.front_back_card_id)
+        self.assertEqual(card.id.hex, self.original_front_back_card_id)
+
+    def test_back_front_id(self):
+        """
+        Card id for the back-back card should stay the same.
+        """
+        card = Card.objects.get(id=self.back_front_card_id)
+        self.assertEqual(card.id.hex, self.original_back_front_card_id)
