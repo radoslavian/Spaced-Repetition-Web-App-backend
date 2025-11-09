3b167881af34b88e2f94a09e9a3610eba7934813
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-11-09 21:01:41 +0100

    Move selected methods and properties from FrontBackBackFront into an abstract class (CardManager).

--- backend/card_types/card_managers/front_back_back_front.py
+++ backend/card_types/card_managers/front_back_back_front.py
@@ -1,26 +1,17 @@
 import json
 from typing import Dict
 
-from cards.models import Card, CardTemplate
+from cards.models import Card
 from .manager_abc import CardManager
 
 
 class FrontBackBackFront(CardManager):
-    def __init__(self, card_note):
-        self.card_note = card_note
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.front_back_card = self._get_card_by_id(
             self.note_metadata.get("front-back-card-id"))
         self.back_front_card = self._get_card_by_id(
             self.note_metadata.get("back-front-card-id"))
-
-    @property
-    def note_metadata(self) -> Dict:
-        if self.card_note.metadata:
-            return json.loads(self.card_note.metadata)
-        return {}
-
-    def _get_card_by_id(self, card_id):
-        return self.card_note.cards.filter(id=card_id).first()
 
     def save_cards(self):
         front = self.card_description.get("front", {})
@@ -45,15 +36,6 @@ class FrontBackBackFront(CardManager):
         card.template = self.get_template()
         card.save()
         return card
-
-    def get_template(self):
-        return CardTemplate.objects.filter(
-            id__exact=self.card_description.get("template")).first()
-
-    @property
-    def card_description(self):
-        return (self.card_note.card_description and
-                json.loads(self.card_note.card_description))
 
     def _save_metadata(self):
         if not all([self.front_back_card, self.back_front_card]):
--- backend/card_types/card_managers/manager_abc.py
+++ backend/card_types/card_managers/manager_abc.py
@@ -1,7 +1,32 @@
+import json
 from abc import ABC, abstractmethod
+from typing import Dict
+
+from cards.models import CardTemplate
 
 
 class CardManager(ABC):
+    def __init__(self, card_note):
+        self.card_note = card_note
+
     @abstractmethod
     def save_cards(self):
         pass
+
+    @property
+    def note_metadata(self) -> Dict:
+        if self.card_note.metadata:
+            return json.loads(self.card_note.metadata)
+        return {}
+
+    def _get_card_by_id(self, card_id):
+        return self.card_note.cards.filter(id=card_id).first()
+
+    @property
+    def card_description(self):
+        return (self.card_note.card_description and
+                json.loads(self.card_note.card_description))
+
+    def get_template(self):
+        return CardTemplate.objects.filter(
+            id__exact=self.card_description.get("template")).first()
