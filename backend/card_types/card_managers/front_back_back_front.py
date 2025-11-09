602a6b18d09f2613ff70abb248fe0d85f665ce7c
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-11-09 17:32:55 +0100

    Refactor FrontBackBackFront.save_cards(), add type hints in ._save_card.

--- backend/card_types/card_managers/front_back_back_front.py
+++ backend/card_types/card_managers/front_back_back_front.py
@@ -13,7 +13,6 @@ class FrontBackBackFront(CardManager):
         self.back_front_card = self._get_card_by_id(
             self.note_metadata.get("back-front-card-id"))
 
-
     @property
     def note_metadata(self) -> Dict:
         if self.card_note.metadata:
@@ -23,25 +22,25 @@ class FrontBackBackFront(CardManager):
     def _get_card_by_id(self, card_id):
         return self.card_note.cards.filter(id=card_id).first()
 
-
     def save_cards(self):
+        front = self.card_description.get("front", {})
+        back = self.card_description.get("back", {})
+
         front_back_card = self.front_back_card or Card()
-        self.front_back_card = self._save_card(
-            card=front_back_card,
-            front=self.card_description["front"]["text"],
-            back=self.card_description["back"]["text"])
+        self.front_back_card = self._save_card(card=front_back_card,
+                                               front=front,
+                                               back=back)
 
         back_front_card = self.back_front_card or Card()
-        self.back_front_card = self._save_card(
-            card=back_front_card,
-            front=self.card_description["back"]["text"],
-            back=self.card_description["front"]["text"])
+        self.back_front_card = self._save_card(card=back_front_card,
+                                               front=back,
+                                               back=front)
 
         self._save_metadata()
 
-    def _save_card(self, card, front, back):
-        card.front = front
-        card.back = back
+    def _save_card(self, card: Card, front: Dict, back: Dict):
+        card.front = front.get("text")
+        card.back = back.get("text")
         card.note = self.card_note
         card.save()
         return card
